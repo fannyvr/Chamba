ba44798270a70444d684709c1c4c1628fefba6e5
Fanny <fannyvr18@gmail.com>
2016-11-12 12:19:26 -0800

    (chore): added HMR to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 });
 
 module.exports = {
+  devtool: 'source-map',
   entry:[
-    './app/index.js'
+    './app/index'
   ],
+  output:{
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
   module:{
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loaders: [ 'react-hot/webpack', 'babel' ]
       },
       {
         test:/\.css$/,
@@ -22,12 +31,19 @@ module.exports = {
       }
     ]
   },
-  output:{
-    filename: 'index_bundle.js',
-    path: __dirname + '/dist' 
+  resolve: {
+    extentions: [ '', '.js' ]
   },
-  plugins:[
-    HtmlWebpackPluginConfig
-  ],
-  devtool: 'source-map'
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    colors: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    HTMLWebpackPluginConfig
+  ]
 };
