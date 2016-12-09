357cf75ba72989726cc67ed77bd8085259d5a835
Fanny <fannyvr18@gmail.com>
2016-12-09 00:22:58 -0800

    feat(nav): log in/out buttons show/hide accordingly

--- app/components/nav/Nav.js
+++ app/components/nav/Nav.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import AuthService from '../../utils/authService';
 import './nav.css';
+
+
+const auth = new AuthService('string', 'string');
 
 class Nav extends Component {
   render(){
+    const show = () => {
+      if(auth.loggedIn()){
+        return <Link to="/logout" className="link"> Log Out </Link>;
+      } 
+      return <Link to="/login" className="link"> Log In </Link>;
+    };
+
     return(
       <div className="nav"> 
         <Link to="/searchjobs" className="link"> Buscar </Link>
         <Link to="/postjobs" className="link"> Publicar </Link>
-        <Link to="/login" className="link"> Log In </Link>
-        <Link to="/logout" className="link"> Log Out </Link>
+        { show() }  
       </div>
-    )
+    );
   }
 }
 
