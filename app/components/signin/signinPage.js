a10767b4306aac758c3ed0a01a70e1ec566ba6d9
Fanny <fannyvr18@gmail.com>
2017-01-18 20:01:21 -0800

    refactor(signin): changed from importing authService class to authService instance

--- app/components/signin/signinPage.js
+++ app/components/signin/signinPage.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import AuthService from '../../utils/authService';
+import auth from '../../utils/authInstance';
 import config from '../../../config';
 
 class SinginPage extends Component {
  
   render(){
-    const auth = new AuthService(config.AUTH0_CLIENT_ID, config.DOMAIN);
-
     return(
       <div>
         <h1>Sign In</h1>
