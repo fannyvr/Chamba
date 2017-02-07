8102236df9396f1dd7b88c8a51b044c0bc5c0646
Fanny <fannyvr18@gmail.com>
2017-02-06 23:36:33 -0800

    chore(redux store): created folder for store.js

--- app/store/store.js
+++ app/store/store.js
@@ -1,16 +1,15 @@
 import { createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { todos } from '../reducers/reducers';
-import { addTodo } from '../actions/actions';
+import { jobs } from '../reducers/reducers';
+import { addJob } from '../actions/actions';
 
 let store = createStore(
-  todos, 
-  [ 'Use Redux' ], 
+  jobs, 
+  [ 'Post Jobs' ], 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 
 
-store.dispatch(addTodo('Read the docs'))
-store.dispatch(addTodo('Read about the middleware'))
+store.dispatch(addJob('Post your jobs'))
 
 export default store;
