733882011eb9d4fdbbed9a072f9fb9f8071378b3
Fanny <fannyvr18@gmail.com>
2017-02-07 21:31:58 -0800

    feat(routes): added route to users list of posted jobs

--- app/components/routes/Routes.js
+++ app/components/routes/Routes.js
@@ -5,6 +5,7 @@ import App from '../app/App';
 import SigninPage from '../signin/signinPage';
 import PostjobsPage from '../post/PostjobsPage';
 import JobsearchPage from '../search/JobsearchPage';
+import MyPostedJobsPage from '../myposts/MyPostedJobsPage';
 import auth from '../../utils/authInstance';
 
 const requireAuth = (nextState, replace)=>{
@@ -23,6 +24,7 @@ const routes = (
     <IndexRoute component={ JobsearchPage } />
     <Route path="/searchjobs" component={ JobsearchPage } />
     <Route path="/postjobs" component={ PostjobsPage } onEnter={ requireAuth } />
+    <Route path="/myposts" component={ MyPostedJobsPage } onEnter={ requireAuth } />
     <Route path="/login" component={ SigninPage } />
     <Route path="/logout" component={ SigninPage } onEnter={ logout } />
     </Route>
