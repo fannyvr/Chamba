c55576ebcdbf530cedf5865a2947c730b0d22d87
Fanny <fannyvr18@gmail.com>
2017-02-07 21:37:04 -0800

    feat(myPostedJobs): added view for users posted jobs

---
+++ app/components/myPosts/MyPostedJobsPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyPosts extends Component { 
+  render(){
+    return(
+      <div>
+        <h1> List of Posted Jobs </h1>
+      </div>
+    )
+  }
+}
+
+export default MyPosts;
