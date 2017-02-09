a541e0f4309ffd22c2a967b3b60e2307be59b9ab
Fanny <fannyvr18@gmail.com>
2017-02-08 23:47:00 -0800

    feat(postJobs): created a form with some fields to enter job info

--- app/components/post/PostjobsPage.js
+++ app/components/post/PostjobsPage.js
@@ -5,6 +5,17 @@ class Post extends Component {
     return(
       <div> 
         <h1>POST</h1>
+        <form>
+          Title: <br /> <input type="text" /> <br />
+          Category: <br /> <input type="text" /> <br />
+          Hourly Salary: <br /> <input type="text" /> <br />
+          Position: <br /> <input type="text" /> <br />
+          Job Description: <br /> <input type="text" /> <br />
+          Contact Info: <br /> <input type="text" /> <br />
+          Aplication: <br /> <input type="text" /> <br />
+          <button> Post </button>
+          <button> Cancel </button>
+        </form>
       </div>
     )
   }
