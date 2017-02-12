3a493ea75e3fc61533b09723e027dd22e50db2b6
Fanny <fannyvr18@gmail.com>
2017-02-11 23:39:44 -0800

    refactor(routes): changed the component of '/postjobs' route

--- app/components/routes/Routes.js
+++ app/components/routes/Routes.js
@@ -3,9 +3,10 @@ import { Router, Route, IndexRoute, browserHistory  } from 'react-router';
 
 import App from '../app/App';
 import SigninPage from '../signin/signinPage';
-import PostjobsPage from '../post/PostjobsPage';
 import JobsearchPage from '../search/JobsearchPage';
 import MyPostedJobsPage from '../myposts/MyPostedJobsPage';
+import PostContainer from '../../containers/PostContainer';
+
 import auth from '../../utils/authInstance';
 
 const requireAuth = (nextState, replace)=>{
@@ -23,7 +24,7 @@ const routes = (
     <Route path="/" component={ App } >
     <IndexRoute component={ JobsearchPage } />
     <Route path="/searchjobs" component={ JobsearchPage } />
-    <Route path="/postjobs" component={ PostjobsPage } onEnter={ requireAuth } />
+    <Route path="/postjobs" component={ PostContainer } onEnter={ requireAuth } />
     <Route path="/myposts" component={ MyPostedJobsPage } onEnter={ requireAuth } />
     <Route path="/login" component={ SigninPage } />
     <Route path="/logout" component={ SigninPage } onEnter={ logout } />
