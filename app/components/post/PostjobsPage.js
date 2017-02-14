c175527c18c70d4b8c28e4baf9ae7412e9a67ad2
Fanny <fannyvr18@gmail.com>
2017-02-13 21:29:08 -0800

    feat(postJobs): changed component to functional component and set the value for each input element to initial state passed in props

--- app/components/post/PostjobsPage.js
+++ app/components/post/PostjobsPage.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 
-class Post extends Component {
-  render(){
-    return(
-      <div> 
-        <h1>POST</h1>
-        <form>
-          Title: <br /> <input type="text" /> <br />
-          Category: <br /> <input type="text" /> <br />
-          Hourly Salary: <br /> <input type="text" /> <br />
-          Position: <br /> <input type="text" /> <br />
-          Job Description: <br /> <input type="text" /> <br />
-          Contact Info: <br /> <input type="text" /> <br />
-          Aplication: <br /> <input type="text" /> <br />
-          <button> Post </button>
-          <button> Cancel </button>
-        </form>
-      </div>
-    )
-  }
+function Post(props) {
+  return(
+    <div> 
+      <h1>POST</h1>
+      <form>
+        Title: <br /> 
+        <input type="text" value={props.title} /> <br />
+        Category: <br /> 
+        <input type="text" value={props.category}  /> <br />
+        Hourly Salary: <br /> 
+        <input type="text" value={props.salary}  /> <br />
+        Position: <br /> 
+        <input type="text" value={props.position}  /> <br />
+        Job Description: <br />
+         <input type="text" value={props.description}  /> <br />
+        Contact Info: <br /> 
+        <input type="text" value={props.contact}  /> <br />
+        Aplication: <br /> 
+        <input type="text" value={props.application}  /> <br />
+        
+        <button onClick={props.onEnter}> Post </button>
+        <button> Cancel </button>
+      </form>
+    </div>
+  )
 }
 
 export default Post;
