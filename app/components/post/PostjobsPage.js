339a8a6f681d427bfc3010994d83c4eb7edb46e4
Fanny <fannyvr18@gmail.com>
2017-02-13 21:50:09 -0800

    feat(postJobs): added an onChange handler that takes a keyword corresponding to the input field

--- app/components/post/PostjobsPage.js
+++ app/components/post/PostjobsPage.js
@@ -6,19 +6,19 @@ function Post(props) {
       <h1>POST</h1>
       <form>
         Title: <br /> 
-        <input type="text" value={props.title} /> <br />
+        <input type="text" value={props.title} onChange={props.onChange('title')}/> <br />
         Category: <br /> 
-        <input type="text" value={props.category}  /> <br />
+        <input type="text" value={props.category} onChange={props.onChange('category')}  /> <br />
         Hourly Salary: <br /> 
-        <input type="text" value={props.salary}  /> <br />
+        <input type="text" value={props.salary} onChange={props.onChange('salary')} /> <br />
         Position: <br /> 
-        <input type="text" value={props.position}  /> <br />
+        <input type="text" value={props.position} onChange={props.onChange('position')} /> <br />
         Job Description: <br />
-         <input type="text" value={props.description}  /> <br />
+         <input type="text" value={props.description} onChange={props.onChange('description')} /> <br />
         Contact Info: <br /> 
-        <input type="text" value={props.contact}  /> <br />
+        <input type="text" value={props.contact} onChange={props.onChange('contact')} /> <br />
         Aplication: <br /> 
-        <input type="text" value={props.application}  /> <br />
+        <input type="text" value={props.application} onChange={props.onChange('application')} /> <br />
         
         <button onClick={props.onEnter}> Post </button>
         <button> Cancel </button>
