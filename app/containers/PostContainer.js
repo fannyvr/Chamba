4348b846e683e8975be9626e99828259648df585
Fanny <fannyvr18@gmail.com>
2017-02-13 21:19:58 -0800

    feat(postJobs): added initial state and passed in props to PostJobs component

--- app/containers/PostContainer.js
+++ app/containers/PostContainer.js
@@ -4,15 +4,36 @@ import PostJobsPage from '../components/post/PostJobsPage';
 class PostContainer extends Component {
   constructor() {
     super();
-    this.state = {};
-  }
+    this.state = {
+      title: '',
+      category: '',
+      salary: '',
+      position: '',
+      description: '',
+      contact: '',
+      application: ''
+    };
+}
   render(){
     return (
       <div> 
-        <PostJobsPage />
+        <PostJobsPage onChange={this.handleChange} 
+                      onEnter={this.handleEnter}
+                      title={this.state.title}
+                      category={this.state.category}
+                      salary={this.state.salary}
+                      position={this.state.position}
+                      description={this.state.description}
+                      contact={this.state.contact}
+                      application={this.state.application}                    
+                      />
       </div>
     )
   }
 }
 
 export default PostContainer;
+
+
+
+
