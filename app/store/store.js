4b6e208bdf91d1ac1b48bf21f303bd946a005fce
Fanny <fannyvr18@gmail.com>
2017-02-16 21:55:32 -0800

    refactor(store): removed action dispatch from store

--- app/store/store.js
+++ app/store/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { jobs } from '../reducers/reducers';
-import { addJob } from '../actions/actions';
 
 let store = createStore(
   jobs, 
@@ -9,7 +8,5 @@ let store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 
-
-store.dispatch(addJob('Post your jobs'))
 
 export default store;
