7114091d63084992b0d158af9635148410979e75
Fanny <fannyvr18@gmail.com>
2017-02-23 23:00:01 -0800

    feat(jobSearch): connected component to global state and rendering any current jobs in the state

--- app/components/search/JobsearchPage.js
+++ app/components/search/JobsearchPage.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Jobs extends Component {
   render(){
     return(
       <div> 
         <h1>JOBS</h1>
+        
+        {this.props.jobs.map((job, index) =>(
+          <div key={index}>
+            <div>Title: {job.title}</div>
+            <div>Category: {job.category}</div>
+            <div>Salary: {job.salary}</div>
+            <div>Position: {job.position}</div>
+            <div>Description: {job.description}</div>
+          </div>))}
       </div>
     )
   }
 }
 
-export default Jobs;
+
+function mapStateToProps(state){
+  return { jobs: state.jobs }
+}
+
+export default connect(mapStateToProps)(Jobs);
