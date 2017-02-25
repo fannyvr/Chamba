86725a6c9a68299a6e6d473df421db0e08307d09
Fanny <fannyvr18@gmail.com>
2017-02-24 22:39:07 -0800

    feat(jobSearch): added click handler to the search container

--- app/containers/JobSearchContainer.js
+++ app/containers/JobSearchContainer.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
+
 import JobSearchPage from '../components/search/JobSearchPage';
 
+
 class JobSearchContainer extends Component{
+ 
+  handleClick(){
+    console.log('details here')
+
+    browserHistory.push('/jobdetails')
+  }
+
   render(){
     return(
-      <JobSearchPage jobs={this.props.jobs} />
+      <JobSearchPage jobs={this.props.jobs}
+                     onClick={this.handleClick} />
     )
   }
 }
