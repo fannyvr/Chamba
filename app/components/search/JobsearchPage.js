1723aed16d2f8d457666b1e8947ea897114701d0
Fanny <fannyvr18@gmail.com>
2017-02-24 19:00:48 -0800

    feat(jobSearch): create a link of every job's title

--- app/components/search/JobsearchPage.js
+++ app/components/search/JobsearchPage.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { Link } from 'react-router';
 
+import JobDetailsPage from '../jobDetails/JobDetailsPage';
 
-class Jobs extends Component {
-  render(){
-    return(
-      <div> 
-        <h1>JOBS</h1>
-        
-        {this.props.jobs.map((job, index) =>(
-          <div key={index}>
-            <div>Title: {job.title}</div>
-            <div>Category: {job.category}</div>
-            <div>Salary: {job.salary}</div>
-            <div>Position: {job.position}</div>
-            <div>Description: {job.description}</div>
-          </div>))}
-      </div>
-    )
-  }
+function JobSearch(props){
+  return(
+    <div> 
+      <h1>JOBS</h1>
+      
+      {props.jobs.map((job, index) =>(
+        <div key={index}>
+          <Link to="/jobdetails" > { job.title } </Link>
+        </div>))}
+    </div>
+  )
 }
 
 
-function mapStateToProps(state){
-  return { jobs: state.jobs }
-}
-
-export default connect(mapStateToProps)(Jobs);
+export default JobSearch;
