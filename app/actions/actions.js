554585e0997dc3204c2bff64c3f989f6165cbacc
Fanny <fannyvr18@gmail.com>
2017-02-25 23:50:47 -0800

    feat(jobSearch): added new action for clicked on jobs

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -3,3 +3,8 @@ import * as constants from '../constants/constants';
 export function postJob(job) {
   return { type: constants.POST_JOB, payload: job }
 }
+
+export function clickedJob(job) {
+  console.log('you clicked on:', job.title)
+  return { type: constants.CLICKED_JOB, payload: job }
+}
