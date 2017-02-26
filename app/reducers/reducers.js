d46bca69fbb76b423714d52e7a7e2a0653b44e61
Fanny <fannyvr18@gmail.com>
2017-02-26 00:39:24 -0800

    feat(jobDetails): created reducer for clicked on job

--- app/reducers/reducers.js
+++ app/reducers/reducers.js
@@ -2,20 +2,30 @@ import { combineReducers } from 'redux';
 import * as constants from '../constants/constants';
 
  function jobs(state = [], action) {
-  switch (action.type) {
+  switch (action.type){
     case constants.POST_JOB:
       return [
         ...state,
         action.payload
-        
       ]
     default:
       return state
   }
 }
 
+
+function activeSearchJob(state = {}, action){
+  switch(action.type){
+    case constants.CLICKED_JOB:
+      return action.payload
+    default: 
+      return state
+  }
+}
+
 const allReducers = combineReducers({
-  jobs
+  jobs,
+  activeSearchJob
 });
 
 export default allReducers;
