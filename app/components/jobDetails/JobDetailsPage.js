37c44c79cef1cc2404020bd093da5913e0a50544
Fanny <fannyvr18@gmail.com>
2017-02-27 18:41:38 -0800

    refactor(jobDetails): refactor component to stateless component

--- app/components/jobDetails/JobDetailsPage.js
+++ app/components/jobDetails/JobDetailsPage.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-
-class JobDetails extends Component{
-  render(){
+function JobDetails (props){
     return(
       <div>
-        <div>{this.props.clickedJobOnSearch.title}</div>
-        <div>{this.props.clickedJobOnSearch.category}</div>
-        <div>{this.props.clickedJobOnSearch.salary}</div>
-        <div>{this.props.clickedJobOnSearch.position}</div>
-        <div>{this.props.clickedJobOnSearch.description}</div>
-        <div>{this.props.clickedJobOnSearch.contact}</div>
-        <div>{this.props.clickedJobOnSearch.application}</div>
+        <div>{props.clickedJobOnSearch.title}</div>
+        <div>{props.clickedJobOnSearch.category}</div>
+        <div>{props.clickedJobOnSearch.salary}</div>
+        <div>{props.clickedJobOnSearch.position}</div>
+        <div>{props.clickedJobOnSearch.description}</div>
+        <div>{props.clickedJobOnSearch.contact}</div>
+        <div>{props.clickedJobOnSearch.application}</div>
       </div>
     )
-  }
 };
 
-function mapStateToProps(state){
-  return { clickedJobOnSearch: state.clickedJobOnSearch }
-}
-
-export default connect(mapStateToProps)(JobDetails);
+export default JobDetails;
