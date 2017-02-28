f08a6784f216c7035eabf111c760af209e29b109
Fanny <fannyvr18@gmail.com>
2017-02-28 00:06:26 -0800

    feat(jobDetails): added className for group of info in the job details

--- app/components/jobDetails/JobDetailsPage.js
+++ app/components/jobDetails/JobDetailsPage.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 import './jobDetails.css';
 
 function JobDetails (props){
-    return(
-      <div>
-        <div id="title"> {props.clickedJobOnSearch.title} </div>
-        <div id="category"> {props.clickedJobOnSearch.category} </div>
-        <div id="salary"> {props.clickedJobOnSearch.salary} </div>
-        <div id="position"> {props.clickedJobOnSearch.position} </div>
-        <div id="description"> {props.clickedJobOnSearch.description} </div>
-        <div id="contact"> {props.clickedJobOnSearch.contact} </div>
-        <div id="application"> {props.clickedJobOnSearch.application} </div>
-      </div>
-    );
+  return(
+    <div>
+      <div id="title"> {props.clickedJobOnSearch.title} </div>
+        <div className="details">
+          <div id="category"> {props.clickedJobOnSearch.category} </div>
+          <div id="salary"> {props.clickedJobOnSearch.salary} </div>
+          <div id="position"> {props.clickedJobOnSearch.position} </div>
+          <div id="description"> {props.clickedJobOnSearch.description} </div>
+          <div id="contact"> {props.clickedJobOnSearch.contact} </div>
+          <div id="application"> {props.clickedJobOnSearch.application} </div>
+        </div>
+    </div>
+  );
 };
 
 export default JobDetails;
