9d8409ffc766a96f9a50237fac9d3226aa486770
Fanny <fannyvr18@gmail.com>
2017-02-27 18:38:30 -0800

    refactor(routes): path '/jobdetails' pointing to container rather than component

--- app/components/routes/Routes.js
+++ app/components/routes/Routes.js
@@ -4,7 +4,7 @@ import { Router, Route, IndexRoute, browserHistory  } from 'react-router';
 import App from '../app/App';
 import SigninPage from '../signin/signinPage';
 import MyPostedJobsPage from '../myposts/MyPostedJobsPage';
-import JobDetailsPage from '../jobDetails/JobDetailsPage';
+import JobDetailsContainer from '../../containers/JobDetailsContainer';
 import PostContainer from '../../containers/PostContainer';
 import JobsearchContainer from '../../containers/JobsearchContainer';
 import auth from '../../utils/authInstance';
@@ -23,12 +23,12 @@ const routes = (
   <Router history={ browserHistory } >
     <Route path="/" component={ App } >
     <IndexRoute component={ JobsearchContainer } />
+    <Route path="/login" component={ SigninPage } />
+    <Route path="/logout" component={ SigninPage } onEnter={ logout } />
     <Route path="/searchjobs" component={ JobsearchContainer } />
     <Route path="/postjobs" component={ PostContainer } onEnter={ requireAuth } />
     <Route path="/myposts" component={ MyPostedJobsPage } onEnter={ requireAuth } />
-    <Route path="/login" component={ SigninPage } />
-    <Route path="/logout" component={ SigninPage } onEnter={ logout } />
-    <Route path="/jobdetails" component={ JobDetailsPage }/>
+    <Route path="/jobdetails" component={ JobDetailsContainer }/>
     </Route>
   </Router>
 );
