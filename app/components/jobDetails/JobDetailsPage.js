14ed003ae87ecdc30a32f6bb132a414fa3c52863
Fanny <fannyvr18@gmail.com>
2017-02-28 22:25:20 -0800

    feat(jobDetails): added clasName and label to every job detail except 'title'. Asigned props to variable created

--- app/components/jobDetails/JobDetailsPage.js
+++ app/components/jobDetails/JobDetailsPage.js
@@ -3,17 +3,16 @@ import React, { Component } from 'react';
 import './jobDetails.css';
 
 function JobDetails (props){
+  const job = props.clickedJobOnSearch;
   return(
     <div>
-      <div id="title"> {props.clickedJobOnSearch.title} </div>
-        <div className="details">
-          <div id="category"> {props.clickedJobOnSearch.category} </div>
-          <div id="salary"> {props.clickedJobOnSearch.salary} </div>
-          <div id="position"> {props.clickedJobOnSearch.position} </div>
-          <div id="description"> {props.clickedJobOnSearch.description} </div>
-          <div id="contact"> {props.clickedJobOnSearch.contact} </div>
-          <div id="application"> {props.clickedJobOnSearch.application} </div>
-        </div>
+      <div id="title"> { job.title } </div>
+          <div id="position" className="details"> <b>Position:</b> { job.position } </div>
+          <div id="salary" className="details"> <b>Salary:</b> { job.salary } </div>
+          <div id="category" className="details"> <b>Category:</b> { job.category } </div>
+          <div id="description" className="details"> <b>Description:</b> { job.description } </div>
+          <div id="contact" className="details"> <b>Contact:</b> { job.contact } </div>
+          <div id="application" className="details"> <b>Application:</b> { job.application } </div>
     </div>
   );
 };
