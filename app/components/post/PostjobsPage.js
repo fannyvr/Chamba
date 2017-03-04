3b850d102430d0856205cc16e6888086f424aebf
Fanny <fannyvr18@gmail.com>
2017-03-03 18:24:19 -0800

    refactor(postJobs): added classNames to the post and cancel buttons

--- app/components/post/PostjobsPage.js
+++ app/components/post/PostjobsPage.js
@@ -14,8 +14,8 @@ function Post(props) {
         <div className="details"> Contact Info: <br/> <input type="text" name="contact" value={props.contact} /> <br/> </div>
         <div className="details"> Aplication: <br/> <input type="text" name="application"  value={props.application} /> <br/> </div>
         
-        <button onClick={props.onEnter}> Post </button>
-        <button> Cancel </button>
+        <button className="postButton" onClick={props.onEnter}> Post </button>
+        <button className="cancelButton"> Cancel </button>
       </form>
     </div>
   );
