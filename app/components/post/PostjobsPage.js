8c89b52a1c28d6b065d02833021468b3d3d06cb5
Fanny <fannyvr18@gmail.com>
2017-03-06 22:30:44 -0800

    refactor(postJobs): changed the Post button type to submit and removed the onclick event. Added onSubmit event to the form and made some fields required

--- app/components/post/PostjobsPage.js
+++ app/components/post/PostjobsPage.js
@@ -5,16 +5,16 @@ function Post(props) {
   return(
     <div> 
       <h1>POST</h1>
-      <form onChange={props.onChange}>
-        <div className="details"> Title: <br/> <input type="text" name="title" value={props.title} required={props.req}/> <br/> </div>
+      <form onSubmit={props.onEnter} onChange={props.onChange}>
+        <div className="details"> Title: <br/> <input type="text" name="title" value={props.title} required/> <br/> </div>
         <div className="details"> Category: <br/> <input type="text" name="category" value={props.category} /> <br/> </div>
-        <div className="details"> Hourly Salary: <br/> <input type="text" name="salary" value={props.salary} /> <br/> </div>
-        <div className="details"> Position: <br/> <input type="text" name="position" value={props.position} /> <br/> </div>
-        <div id="description"> Job Description: <br/> <textarea type="text" name="description" value={props.description} /> <br/> </div>
-        <div className="details"> Contact Info: <br/> <input type="text" name="contact" value={props.contact} /> <br/> </div>
+        <div className="details"> Hourly Salary: <br/> <input type="text" name="salary" value={props.salary} required/> <br/> </div>
+        <div className="details"> Position: <br/> <input type="text" name="position" value={props.position} required/> <br/> </div>
+        <div id="description"> Job Description: <br/> <textarea type="text" name="description" value={props.description} required/> <br/> </div>
+        <div className="details"> Contact Info: <br/> <input type="text" name="contact" value={props.contact} required/> <br/> </div>
         <div className="details"> Aplication: <br/> <input type="text" name="application"  value={props.application} /> <br/> </div>
         
-        <button type="button" className="postButton" onClick={props.onEnter}> Post </button>
+        <button type="submit" className="postButton" > Post </button>
         <button type="button" className="cancelButton" onClick={props.onCancel}> Cancel </button>
       </form>
     </div>
