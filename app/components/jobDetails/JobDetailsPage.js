cbdf6208bb5df63b89730cc7e95d9a3ce75233c5
Fanny <fannyvr18@gmail.com>
2017-03-08 22:59:40 -0800

    syntax(jobDetails): added space and indentations between divs

--- app/components/jobDetails/JobDetailsPage.js
+++ app/components/jobDetails/JobDetailsPage.js
@@ -6,13 +6,43 @@ function JobDetails (props){
   const job = props.clickedJobOnSearch;
   return(
     <div>
-      <div id="title"> { job.title } </div>
-        <div id="jposition" className="detail"> <b> Position: </b> <br/> { job.position } </div>
-        <div id="jsalary" className="detail"> <b> Salary: </b> <br/> { job.salary } </div>
-        { job.category.length > 0 && <div id="jcategory" className="detail"> <b> Category: </b> <br/> { job.category } </div> }
-        <div id="jdescription"> <b> Description: </b> <br/> { job.description } </div>
-        <div id="jcontact" className="detail"> <b> Contact: </b> <br/> { job.contact } </div>
-        { job.application.length > 0 && <div id="japplication" className="detail"> <b> Application: </b> <br/> { job.application } </div> }
+      <div id="title"> 
+        { job.title } 
+      </div>
+
+      <div className="detail"> 
+        <b> Position: </b> <br/> 
+        { job.position } 
+      </div>
+
+      <div className="detail"> 
+        <b> Salary: </b> <br/> 
+        { job.salary } 
+      </div>
+
+      { job.category.length > 0 && 
+        <div className="detail"> 
+          <b> Category: </b> <br/> 
+          { job.category } 
+        </div> 
+      }
+
+      <div id="jdescription"> 
+        <b> Description: </b> <br/> 
+        { job.description } 
+      </div>
+
+      <div className="detail"> 
+        <b> Contact: </b> <br/> 
+        { job.contact } 
+      </div>
+
+      { job.application.length > 0 && 
+        <div className="detail"> 
+          <b> Application: </b> <br/> 
+          { job.application } 
+        </div> 
+      }
     </div>
   );
 };
