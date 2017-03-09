983063a013d6788de22298a93bf34d032b9267c7
Fanny <fannyvr18@gmail.com>
2017-03-08 23:18:57 -0800

    syntax(jobDetails): removed white lines

--- app/components/jobDetails/JobDetailsPage.js
+++ app/components/jobDetails/JobDetailsPage.js
@@ -4,39 +4,34 @@ import './jobDetails.css';
 
 function JobDetails (props){
   const job = props.clickedJobOnSearch;
+  
   return(
     <div>
       <div id="title"> 
         { job.title } 
       </div>
-
       <div className="detail"> 
         <b> Position: </b> <br/> 
         { job.position } 
       </div>
-
       <div className="detail"> 
         <b> Salary: </b> <br/> 
         { job.salary } 
       </div>
-
       { job.category.length > 0 && 
         <div className="detail"> 
           <b> Category: </b> <br/> 
           { job.category } 
         </div> 
       }
-
       <div id="jdescription"> 
         <b> Description: </b> <br/> 
         { job.description } 
       </div>
-
       <div className="detail"> 
         <b> Contact: </b> <br/> 
         { job.contact } 
       </div>
-
       { job.application.length > 0 && 
         <div className="detail"> 
           <b> Application: </b> <br/> 
