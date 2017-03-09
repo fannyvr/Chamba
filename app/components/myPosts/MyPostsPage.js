d1a6578b42b27cb3e5f41694ed43307612373d71
Fanny <fannyvr18@gmail.com>
2017-03-08 23:28:55 -0800

    syntax(myPosts): added spaces and fixed indentation

--- app/components/myPosts/MyPostsPage.js
+++ app/components/myPosts/MyPostsPage.js
@@ -6,16 +6,18 @@ function MyPosts(props){
   return(
     <div>
       <h1> My Posts </h1>
-        <ul id="postsList">
-          {props.jobs.map((job, index)=>(
-            <li className="postItem" key={index} onClick={()=>props.onClick(job)}> 
-              {job.title} 
-            </li>
-          ))}
-        </ul>
+      <ul id="postsList">
+        { props.jobs.map( (job, index) => (
+          <li className="postItem" 
+              key={ index } 
+              onClick={ () => props.onClick(job) }
+          > 
+            { job.title } 
+          </li> ))
+        }
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default MyPosts;
-
