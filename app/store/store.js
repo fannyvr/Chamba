20e5cc536254efe60e8f0bc34f202cb42b9073db
Fanny <fannyvr18@gmail.com>
2017-03-11 17:38:55 -0800

    feat(redux store): passed initial state to createStore

--- app/store/store.js
+++ app/store/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { Map } from 'immutable';
+
 import allReducers  from '../reducers/reducers';
 
 
 let store = createStore(
   allReducers,
+  Map(),
   compose(
     applyMiddleware(thunkMiddleware),
     window.devToolsExtension ? window.devToolsExtension() : undefined
