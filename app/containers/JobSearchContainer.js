c5d3807670f9822c3f9c5b348666c84fd2385d51
Fanny <fannyvr18@gmail.com>
2017-03-11 17:45:38 -0800

    refactor(jobSearch): refactor mapStateToProps to using immutable methods

--- app/containers/JobSearchContainer.js
+++ app/containers/JobSearchContainer.js
@@ -29,7 +29,9 @@ class JobSearchContainer extends Component{
 };
 
 function mapStateToProps(state){
-  return { jobs: state.jobs };
+  let jobs;
+  if(state.get('jobs')) jobs = state.get('jobs').toJS();
+  return { jobs };
 };
 
 function matchDispatchToProps(dispatch){
