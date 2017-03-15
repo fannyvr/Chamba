25431448f1e2062295217c511c350e22d4f52f70
Fanny <fannyvr18@gmail.com>
2017-03-15 16:26:30 -0700

    test(reducers): importing constants and reducers and added test for activeJob reducer

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -1,41 +1,33 @@
 import { List, Map, fromJS } from 'immutable';
-import {expect} from 'chai';
-import {jobList} from '../app/reducers/reducers';
+
+import * as types from '../app/constants/constants';
+import * as reducer from '../app/reducers/reducers';
+
+const payload = {
+  title:'Need Cook!',
+  position: 'Cook',
+  salary:'$10',
+  category:'Food',
+  description:'Position Description Here!',
+  contact:'555-555-5555',
+  application:'appUrl.com'
+};
 
 describe('reducer', () => {
 
   it('handles POST_JOB', () => {
     const initState = List();
-    const action = { 
-      type: 'POST_JOB', 
-      payload: {
-        title:'Need Cook!',
-        position: 'Cook',
-        salary:'$10',
-        category:'Food',
-        description:'Position Description Here!',
-        contact:'555-555-5555',
-        application:'appUrl.com'
-      } 
-    };
-    const nextState = jobList(initState, action);
+    const action = { type: types.POST_JOB, payload: payload };
+    const nextState = reducer.jobList(initState, action);
   
-    nextState.should.equal(fromJS(
-      [{
-        title:'Need Cook!',
-        position: 'Cook',
-        salary:'$10',
-        category:'Food',
-        description:'Position Description Here!',
-        contact:'555-555-5555',
-        application:'appUrl.com'
-      }]
-    ));
+    nextState.should.equal(fromJS( [ payload ] ));
+  });
+
+  it('handles CLICKED_JOB', () => {
+    const initState = Map();
+    const action = { type: types.CLICKED_JOB, payload: payload };
+    const nextState = reducer.activeJob(initState, action);
+
+    nextState.should.equal(fromJS( payload ));
   });
 });
-
-
-
-
-
-
