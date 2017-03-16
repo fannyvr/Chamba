4ed8ed31f0402478e4bbedf25bf41c8255749cbc
Fanny <fannyvr18@gmail.com>
2017-03-15 19:37:06 -0700

    test(actions): created test for postJob and clickedJob actions

---
+++ test/frontend/actions_spec.js
@@ -0,0 +1,36 @@
+import * as types from '../../app/constants/constants';
+import * as actions from '../../app/actions/actions';
+import { expect } from 'chai'; 
+
+const payload = {
+  title:'Need Cook!',
+  position: 'Cook',
+  salary:'$10',
+  category:'Food',
+  description:'Position Description Here!',
+  contact:'555-555-5555',
+  application:'appUrl.com'
+};
+
+describe('actions', () => {
+
+  it('should create an action to add a job', () => {
+    const action = actions.postJob(payload);
+    const expectedAction = { 
+      type: types.POST_JOB, 
+      payload 
+    };
+
+    expect(action).to.deep.equal(expectedAction);
+  });
+
+  it('should create an action to set the clicked on job', () => {
+    const action = actions.clickedJob(payload);
+    const expectedAction = { 
+      type: types.CLICKED_JOB, 
+      payload
+    };
+
+    expect(action).to.deep.equal(expectedAction);
+  });
+});
