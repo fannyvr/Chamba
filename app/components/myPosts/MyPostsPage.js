55a72c6ffbe08991fb3ebc18a9fd457455a9b761
Fanny <fannyvr18@gmail.com>
2017-03-18 23:46:50 -0700

    feat(myPosts): added nested ul to display date above job title

--- app/components/myPosts/MyPostsPage.js
+++ app/components/myPosts/MyPostsPage.js
@@ -6,13 +6,18 @@ const MyPosts = (props) => (
   <div>
     <h1> My Posts </h1>
     <ul id="postsList">
-      { props.jobs.map( (job, index) => (
-        <li className="postItem" 
-            key={ index } 
-            onClick={ () => props.onClick(job) }
-        > 
-          { job.title } {job.date}
-        </li> ))
+      {props.jobs.map( (job, index) => (
+        <li key={index} className="date"> 
+          {job.date} 
+          <ul id="jobTitle">
+            <li className="postItem" 
+              key={index}
+              onClick={ () => props.onClick(job) } 
+            >
+              {job.title} 
+            </li> 
+          </ul>
+        </li>))
       }
     </ul>
   </div>
