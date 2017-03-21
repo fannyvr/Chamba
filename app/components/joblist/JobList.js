57e7f2e739097f554a19d71f9a40cd802031c832
Fanny <fannyvr18@gmail.com>
2017-03-20 19:18:06 -0700

    feat(JobList): created list component and passing jobTitles component as child

--- app/components/joblist/JobList.js
+++ app/components/joblist/JobList.js
@@ -1,15 +1,21 @@
 import React from 'react';
+
+import JobTitles from './JobTitles';
 import './joblist.css';
 
 const JobList = (props) => (
-  <ul id="jobList">
-    <li className="jobTitle" 
-        key={ props.index }
-        onClick={ () => props.onClick(props.job) } 
-    >
-      {props.title} 
-    </li> 
-  </ul>
+  <ul id="list">
+      { props.jobs.map( (job, index) => (
+        <li key={index} className="date"> 
+          {job.date} 
+          <JobTitles key={index} 
+                     onClick={props.onClick} 
+                     title={job.title}
+                     job={job} 
+          />
+        </li>))
+      }
+    </ul>
 );
 
 export default JobList;
