6d3a741011b46e269057e1a30de458c0f137ac34
Fanny <fannyvr18@gmail.com>
2017-03-20 22:46:34 -0700

    feat(back routes): using koa-router, route to post a job was created

---
+++ server/routes/routes.js
@@ -0,0 +1,11 @@
+import parse from 'co-body';
+const router = require('koa-router')();
+import job from '../controllers/jobController'
+const routes = (app) => {
+  app.use(router.routes());
+
+  router.post('/api/postjobs',job);
+
+}
+
+export default routes;
