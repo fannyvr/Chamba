302bc87f99037dde155ae9207206925e395d4eb0
Fanny <fannyvr18@gmail.com>
2017-03-20 19:24:36 -0700

    refactor(jobSearch): passing JobList component as a child with props

--- app/components/search/JobsearchPage.js
+++ app/components/search/JobsearchPage.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import JobList from '../joblist/JobList';
 
-import './jobSearchPage.css';
 
 const JobSearch = (props) => (
   <div> 
     <h1>JOBS</h1>
-    <ul id="searchList">
-      {props.jobs.map( (job, index) => (
-        <li key={index} className="date"> 
-          {job.date} 
-          <JobList key={index} 
-                     onClick={props.onClick} 
-                     title={job.title}
-                     job={job} 
-            />
-        </li> ))
-      }
-    </ul>
+    <JobList jobs={props.jobs} 
+             onClick={props.onClick}
+    />
   </div>
 );
 
