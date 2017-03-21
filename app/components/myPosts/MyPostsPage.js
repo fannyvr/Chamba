bd36aff1050b8b212ccca036a1331ae451e0742d
Fanny <fannyvr18@gmail.com>
2017-03-20 19:23:44 -0700

    refactor(myPosts): passing JobList component as a child with props

--- app/components/myPosts/MyPostsPage.js
+++ app/components/myPosts/MyPostsPage.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import JobList from '../joblist/JobList';
-import './myPostsPage.css';
 
 const MyPosts = (props) => (
   <div>
     <h1> My Posts </h1>
-    <ul id="postsList">
-      { props.jobs.map( (job, index) => (
-         <li key={index} className="postDate"> 
-          {job.date} 
-          <JobList key={index} 
-                   onClick={props.onClick} 
-                   title={job.title}
-                   job={job} 
-          />
-        </li>))
-      }
-    </ul>
+    <JobList jobs={props.jobs} 
+             onClick={props.onClick}
+    />
   </div>
 );
 
