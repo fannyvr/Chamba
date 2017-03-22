cb1bd9fe33e70ab509ac1f50a9e7e84e592f8624
Fanny <fannyvr18@gmail.com>
2017-03-22 16:34:28 -0700

    refactor(actions): added return keywor

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import * as constants from '../constants/constants';
 
-export const clickedJob = (job) => 
-  { type: constants.CLICKED_JOB, payload: job }
+export const clickedJob = (job) => {
+  return { type: constants.CLICKED_JOB, payload: job };
+};
 
-export const postJobSuccess = (job) =>
-  { type: constants.POST_JOB, payload: job }
+export const postJobSuccess = (job) =>{
+  return { type: constants.POST_JOB, payload: job };
+};
+
 
 export const postJob = (job) => {
   return (dispatch) => {
