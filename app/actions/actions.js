b211a25cb5f995175bebbdfc8bae66957265aebe
Fanny <fannyvr18@gmail.com>
2017-03-21 21:22:37 -0700

    syntax(actions): fixed indentation and removed white lines

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -1,20 +1,16 @@
 import axios from 'axios';
-
 import * as constants from '../constants/constants';
 
-
 export const clickedJob = (job) => 
-    { type: constants.CLICKED_JOB, payload: job }
-
+  { type: constants.CLICKED_JOB, payload: job }
 
 export const postJobSuccess = (job) =>
   { type: constants.POST_JOB, payload: job }
-
 
 export const postJob = (job) => {
   return (dispatch) => {
     return axios.post('/api/postjobs', job)
       .then((res) => dispatch(postJobSuccess(job)))
-      .catch((err) => {console.log(err)})
+      .catch((err) => {console.log(err)});
   };
 }; 
