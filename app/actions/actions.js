f4bb4b2cb2227461f1f3485ea2a538bf85290514
Fanny <fannyvr18@gmail.com>
2017-03-21 20:52:28 -0700

    syntax(actions): changed to ES6 syntax

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -1,9 +1,13 @@
+import axios from 'axios';
+
 import * as constants from '../constants/constants';
 
-export function postJob(job) {
-  return { type: constants.POST_JOB, payload: job }
-};
 
-export function clickedJob(job) {
-  return { type: constants.CLICKED_JOB, payload: job }
-};
+export const clickedJob = (job) => 
+    { type: constants.CLICKED_JOB, payload: job }
+
+
+export const postJobSuccess = (job) =>
+  { type: constants.POST_JOB, payload: job }
+
+
