ececf5a518b9c4040960ee7b16db6e99b17e2c88
Fanny <fannyvr18@gmail.com>
2017-03-28 23:19:54 -0700

    feat(back routes): adde jwt check

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,6 @@
+import koaJwt from 'koa-jwt';
+import dotenv from 'dotenv'; 
+dotenv.config();
 import parse from 'co-body';
 import koaRouter from 'koa-router';
 const router = koaRouter();
@@ -6,6 +9,11 @@ import {job, getJobs} from '../controllers/jobController'
 
 const routes = (app) => {
   app.use(router.routes());
+
+  const authCheck = koaJwt({
+  secret: new Buffer(process.env.AUTH0_SECRET, 'base64'),
+  audience:  process.env.AUTH0_CLIENT_ID
+});
 
   router.post('/api/postjobs', job);
   router.get('/api/searchjobs', getJobs)
