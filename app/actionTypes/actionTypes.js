989fa7201db9f8119ae956c1c38fe96ad886e9f3
Fanny <fannyvr18@gmail.com>
2017-04-02 18:10:34 -0700

    feat(actionTypes): added actionTypes for login and logout actions

--- app/actionTypes/actionTypes.js
+++ app/actionTypes/actionTypes.js
@@ -1,3 +1,7 @@
 export const GET_JOBS = 'GET_JOBS';
 export const POST_JOB = 'POST_JOB';
 export const CLICKED_JOB = 'CLICKED_JOB';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
