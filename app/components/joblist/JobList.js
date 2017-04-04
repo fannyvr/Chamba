4d2856c531a133c614d56805c844985f1be7abc1
Fanny <fannyvr18@gmail.com>
2017-04-03 23:21:01 -0700

    refactor(jobList): using es6 object deconstructoring

--- app/components/joblist/JobList.js
+++ app/components/joblist/JobList.js
@@ -3,19 +3,22 @@ import React from 'react';
 import JobTitles from './JobTitles';
 import './joblist.css';
 
-const JobList = (props) => (
-  <ul id="list">
-      { props.jobs.map( (job, index) => (
+const JobList = (props) => {
+  const { jobs, onClick } = props;
+  return (
+    <ul id="list">
+      { jobs.map( (job, index) => (
         <li key={index} className="date"> 
           {job.date} 
           <JobTitles key={index} 
-                     onClick={props.onClick} 
+                     onClick={onClick} 
                      title={job.title}
                      job={job} 
           />
         </li>))
       }
     </ul>
-);
+  )
+};
 
 export default JobList;
--- app/components/joblist/JobTitles.js
+++ app/components/joblist/JobTitles.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 import './joblist.css';
 
-const JobTitles = (props) => (
-  <ul id="jobList">
-    <li className="jobTitle" 
-        key={ props.index }
-        onClick={ () => props.onClick(props.job) } 
-    >
-      {props.title} 
-    </li> 
-  </ul>
-);
+const JobTitles = (props) => {
+  const { index, onClick, job, title } = props;
+  return ( 
+    <ul id="jobList">
+      <li className="jobTitle" 
+          key={ index }
+          onClick={ () => onClick(job) } 
+      >
+        {title} 
+      </li> 
+    </ul>
+  );
+};
 
 export default JobTitles;
