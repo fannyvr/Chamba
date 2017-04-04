2e1ecbcbd35c4a8b90d902d696e22a4f250b1e76
Fanny <fannyvr18@gmail.com>
2017-04-03 23:17:40 -0700

    refactor(jobSearch): changed to using object deconstructoring

--- app/components/search/JobsearchPage.js
+++ app/components/search/JobsearchPage.js
@@ -2,13 +2,16 @@ import React from 'react';
 import JobList from '../joblist/JobList';
 
 
-const JobSearch = (props) => (
-  <div> 
-    <h1>JOBS</h1>
-    <JobList jobs={props.jobs} 
-             onClick={props.onClick}
-    />
-  </div>
-);
+const JobSearch = (props) => {
+  const { jobs, onClick } = props;
+  return(
+    <div> 
+      <h1>JOBS</h1>
+      <JobList jobs={jobs} 
+               onClick={onClick}
+      />
+    </div>
+  );
+};
 
 export default JobSearch;
