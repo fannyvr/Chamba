0fb67c573a883d7aa05fe33845ee32a2d5ad867b
Fanny <fannyvr18@gmail.com>
2017-04-04 23:32:45 -0700

    feat(appContainer): check if user is logged in before it renders to set the state and render correct buttons on navbar. Rendering NavContainer instead of the component

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import Nav from '../components/nav/Nav';
+import lock from '../utils/authInstance';
+import NavContainer from './NavContainer';
+import { logInSuccess } from '../actions/actions';
 
 class AppContainer extends Component {
+  
+  componentWillMount(){
+    if(lock.loggedIn()){
+      this.props.dispatch(logInSuccess({ loggedIn: true }));
+    }
+  }
+
   render(){
     return(
       <div>
-        <Nav />
+        <NavContainer />
         { this.props.children }
       </div>
     );
   }
 };
 
-export default AppContainer;
+export default connect()(AppContainer);
