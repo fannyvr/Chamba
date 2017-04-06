732548e1e55b6ae47f9d5f431c1dc3df73942a8f
Fanny <fannyvr18@gmail.com>
2017-04-05 23:24:00 -0700

    refactor(Routes): removed helper functions

--- app/components/routes/Routes.js
+++ app/components/routes/Routes.js
@@ -7,28 +7,17 @@ import MyPostsContainer from '../../containers/MyPostsContainer';
 import JobDetailsContainer from '../../containers/JobDetailsContainer';
 import PostContainer from '../../containers/PostContainer';
 import JobsearchContainer from '../../containers/JobsearchContainer';
-import auth from '../../utils/authInstance';
-
-const requireAuth = (nextState, replace)=>{
-  if( !auth.loggedIn() ){
-    replace({pathname:'/login'})
-  }
-};
-
-const logout = () => {
-  return auth.logout();
-};
 
 const routes = (
   <Router history={ browserHistory } >
     <Route path="/" component={ AppContainer } >
     <IndexRoute component={ JobsearchContainer } />
     <Route path="/login" component={ SigninPage } />
-    <Route path="/logout" component={ SigninPage } onEnter={ logout } />
+    <Route path="/logout" component={ SigninPage } />
     <Route path="/searchjobs" component={ JobsearchContainer } />
-    <Route path="/postjobs" component={ PostContainer } onEnter={ requireAuth } />
-    <Route path="/myposts" component={ MyPostsContainer } onEnter={ requireAuth } />
-    <Route path="/jobdetails" component={ JobDetailsContainer }/>
+    <Route path="/postjobs" component={ PostContainer } />
+    <Route path="/myposts" component={ MyPostsContainer } />
+    <Route path="/jobdetails" component={ JobDetailsContainer } />
     </Route>
   </Router>
 );
