eebfdfc48ec1e022db32213107fdd5dce0e5b266
Fanny <fannyvr18@gmail.com>
2017-04-05 22:47:53 -0700

    refactor(appContainer): removed componentWillMount method and diconnected container from global state

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-import lock from '../utils/authInstance';
 import NavContainer from './NavContainer';
-import { logInSuccess } from '../actions/actions';
 
 class AppContainer extends Component {
-  
-  componentWillMount(){
-    if(lock.loggedIn()){
-      this.props.dispatch(logInSuccess({ loggedIn: true }));
-    }
-  }
-
   render(){
     return(
       <div>
@@ -23,4 +13,4 @@ class AppContainer extends Component {
   }
 };
 
-export default connect()(AppContainer);
+export default AppContainer;
