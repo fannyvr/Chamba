9a138c6cc0b1c08862acce9d2ec05ccfafa9ce7a
Fanny <fannyvr18@gmail.com>
2017-04-05 21:59:59 -0700

    refactor(SigninPage): importing middleware and passing it to onClick event to show auth0 lock

--- app/components/signin/signinPage.js
+++ app/components/signin/signinPage.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import auth from '../../utils/authInstance';
 import config from '../../../config';
+import { showLock } from '../../utils/middleware';
 
 class SinginPage extends Component {
- 
   render(){
     return(
       <div>
         <h1>Sign In</h1>
-        <button onClick={ auth.login }>Entar</button>
+        <button onClick={ showLock }>Entar</button>
       </div>
     );
   }
