84da8363f5e0b98e423f78c47bde479ebceb33df
Fanny <fannyvr18@gmail.com>
2017-04-07 16:51:22 -0700

    refactor(lockInstance): changed callback url

--- app/utils/authInstance.js
+++ app/utils/authInstance.js
@@ -3,7 +3,7 @@ import config from '../../config';
 
 const lock = new Auth0Lock(config.AUTH0_CLIENT_ID, config.DOMAIN,{
   auth: {
-    redirectUrl: 'http://localhost:8080/postjobs',
+    redirectUrl: 'http://localhost:3000/postjobs',
     responseType: 'token'
   }
 });
