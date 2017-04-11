019eaf6c435526f4c72791add7801823a80cf712
Fanny <fannyvr18@gmail.com>
2017-04-11 14:35:11 -0700

    syntax(actions): added comments, spaces, semicolons and removed paranthesis from one param functions

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -1,69 +1,79 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+
 import * as types from '../actionTypes/actionTypes';
 import lock from '../utils/authInstance';
-import {browserHistory} from 'react-router';
 
-export const clickedJob = (job) => {
+/* 
+  jobs 
+*/
+export const clickedJob = job => {
   return { type: types.CLICKED_JOB, payload: job };
 };
 
-export const postJobSuccess = (job) => {
+export const postJobSuccess = job => {
   return { type: types.POST_JOB, payload: job };
 };
 
-export const postJob = (job) => {
-  return (dispatch) => {
-    return axios.post('/api/postjobs', job)
-      .then((res) => dispatch(postJobSuccess(job)))
-      .catch((err) => {console.log(err)});
+export const postJob = job => {
+  return dispatch => {
+    return axios.post( '/api/postjobs', job )
+      .then( res => dispatch( postJobSuccess(job) ))
+      .catch( err => { console.log( err ) });
   };
 }; 
 
-export const getJobsSuccess = (jobs) => {
+export const getJobsSuccess = jobs => {
   return { type: types.GET_JOBS, payload: jobs };
 };
 
 export const getJobs = () => {
-  return (dispatch) => {
-    return axios.get('/api/searchjobs')
-      .then((res) => {dispatch(getJobsSuccess(res.data))})
-      .catch((err) => {console.log(err)})
+  return dispatch => {
+    return axios.get( '/api/searchjobs' )
+      .then( res => dispatch( getJobsSuccess( res.data ) ))
+      .catch( err => { console.log( err ) });
   };
 };
 
-export const logInSuccess = (loggedIn) => {
-  return {type: types.LOGIN_SUCCESS, payload: loggedIn };
+/*
+  login
+*/
+export const logInSuccess = loggedIn  => {
+  return { type: types.LOGIN_SUCCESS, payload: loggedIn };
 };
 
-export const logInFail = (loggedIn) => {
+export const logInFail = loggedIn => {
   return { type: types.LOGIN_FAILURE, payload: loggedIn };
 };
 
-export const logOutSuccess = (loggedIn) => {
+export const login = () => {
+  return dispatch => { 
+    lock.on( 'authenticated', authResult => {
+      lock.getUserInfo( authResult.accessToken, ( err, profile ) => {
+        if( err ){
+          return dispatch( logInFail({ loggedIn: false }) );
+        }
+        localStorage.setItem( 'profile', JSON.stringify( profile ) );
+        localStorage.setItem( 'id_token', authResult.idToken );
+      });
+      browserHistory.replace( '/postjobs' );
+    });
+    return dispatch( logInSuccess({ loggedIn:true }) );
+  };
+};
+
+/*
+  logout
+*/
+export const logOutSuccess = loggedIn => {
   return { type: types.LOGOUT_SUCCESS, payload: loggedIn };
 };
 
-export const login = () => {
-  return(dispatch) => { 
-    lock.on('authenticated', (authResult) => {
-      lock.getUserInfo(authResult.accessToken, (err, profile) => {
-        if(err){
-          return dispatch(logInFail({ loggedIn: false }))
-        }
-        localStorage.setItem('profile', JSON.stringify(profile));
-        localStorage.setItem('id_token', authResult.idToken);
-      })
-      browserHistory.replace('/postjobs')
-    })
-    return dispatch(logInSuccess({ loggedIn:true }))
-  }
-};
-
 export const logout = () => {
-  return(dispatch) => {
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('profile');
-    return dispatch(logOutSuccess({ loggedIn: false }));
+  return dispatch => {
+    localStorage.removeItem( 'id_token' );
+    localStorage.removeItem( 'profile' );
+    return dispatch( logOutSuccess({ loggedIn: false }) );
   };
 };
 
