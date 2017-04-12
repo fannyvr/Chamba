69a83821738b4ffaa7c52bdd9612f0b51c540685
Fanny <fannyvr18@gmail.com>
2017-04-11 22:08:02 -0700

    syntax(jobSearch): removed paranthesis from function param and added spaces between brackets

--- app/components/search/JobsearchPage.js
+++ app/components/search/JobsearchPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
+
 import JobList from '../joblist/JobList';
 
 
-const JobSearch = (props) => {
+const JobSearch = props => {
   const { jobs, onClick } = props;
   return(
     <div> 
       <h1>JOBS</h1>
-      <JobList jobs={jobs} 
-               onClick={onClick}
+      <JobList jobs={ jobs } 
+               onClick={ onClick }
       />
     </div>
   );
