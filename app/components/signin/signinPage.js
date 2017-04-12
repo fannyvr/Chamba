ef6c243f53ee1c211c92e2076932cd8b086a642f
Fanny <fannyvr18@gmail.com>
2017-04-11 22:06:05 -0700

    syntax(signin): brake lines and indentation

--- app/components/signin/signinPage.js
+++ app/components/signin/signinPage.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+
 import auth from '../../utils/authInstance';
 import config from '../../../config';
 import { showLock } from '../../utils/middleware';
 
-class SinginPage extends Component {
+class SinginPage extends Component{
   render(){
     return(
       <div>
         <h1>Sign In</h1>
-        <button onClick={ showLock }>Entar</button>
+        <button onClick={ showLock }> 
+          Entar 
+        </button>
       </div>
     );
   }
 };
 
-export default SinginPage;
+export default SinginPage;    
