0ebbcec6b8725e53a6ec57d2ef26a75cc6e11ed0
Fanny <fannyvr18@gmail.com>
2017-04-11 17:22:34 -0700

    syntax(actionTypes): added comments

--- app/actionTypes/actionTypes.js
+++ app/actionTypes/actionTypes.js
@@ -1,7 +1,17 @@
+/*
+  jobs
+*/
 export const GET_JOBS = 'GET_JOBS';
 export const POST_JOB = 'POST_JOB';
 export const CLICKED_JOB = 'CLICKED_JOB';
-export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+
+/*
+  login
+*/
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+/*
+  logout
+*/
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
