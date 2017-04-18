aec61ab87ce78a2c596cf5063a90b47ba29419de
Fanny <fannyvr18@gmail.com>
2017-04-17 21:18:13 -0700

    feat(userController): deconstructing the request body and querying db to check if user exists and send it back otherwise save new user and send it back

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,27 @@
+import User from '../models/user';
+
 export const setUser = async ( ctx, next ) => {
-  console.log( 'DATA!', ctx.request.body );
-  ctx.body = 'got it';
-  ctx.status = 200;
+  const { email, name, picture, nickname, identities } = ctx.request.body;
+  const user = {
+    name: name,
+    email: email,
+    picture: picture,
+    nickname: nickname,
+    userId: identities[0].user_id
+  };
+
+  try{
+    let gotUser = await User.findOne( { email: email } );
+    
+    if( !gotUser ){
+      const newUser = new User( user );   
+      ctx.body = await newUser.save();
+      ctx.status = 200;
+    }else{
+      ctx.body = gotUser;
+      ctx.status = 200;
+    }
+  }catch( err ){
+    err => ctx.status = 500;
+  }
 };
