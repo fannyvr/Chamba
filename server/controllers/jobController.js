56684f3169f2eab6013892eaea2eb383568cedf1
Fanny <fannyvr18@gmail.com>
2017-04-17 23:15:38 -0700

    refactor(jobController): wraped statements with try/catch

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -4,15 +4,20 @@ export const job = async ( ctx, next ) => {
   const data = ctx.request.body;
   const job = new Job( data );
 
-  job.save( err => {
-    if( err ) throw err;
-    console.log( 'job created!' );
-  });
-
-  ctx.status = 200;
+  try{
+    job.save();
+    ctx.body = 'Job has been Posted!';
+    ctx.status = 200;
+  }catch( err ){
+    err => ctx.status = 500;
+  }
 };
 
 export const getJobs = async ( ctx, next ) => {
-  ctx.body =  await Job.find( {} ).exec(); 
-  ctx.status = 200;
+  try{
+    ctx.body =  await Job.find( {} ).exec(); 
+    ctx.status = 200;
+  }catch( err ){
+    err => ctx.status = 500;
+  }
 };
