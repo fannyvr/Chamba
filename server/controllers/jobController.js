6a55b67db76105bc0d41b5fb8495b13a76a4a5e0
Fanny <fannyvr18@gmail.com>
2017-04-24 19:36:38 -0700

    chore(jobController): added logic to check if user exists on db by userId, if so, job is saved to Job list and to User's array of jobs

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,13 +1,20 @@
 import Job from '../models/job';
+import User from '../models/user';
 
 export const postJob = async ( ctx, next ) => {
-  const data = ctx.request.body;
-  const job = new Job( data );
-
   try{
-    job.save();
+    const data = ctx.request.body;
+    const job = new Job( data.job );
+    let currentUser = await User.findOne( { userId: data.userId } );
+    
+    if( currentUser ){
+      let savedJob = job.save();
+      currentUser.jobs.push( await savedJob );
+      currentUser.save();
+    }
     ctx.body = 'Job has been Posted!';
     ctx.status = 200;
+    
   }catch( err ){
     err => ctx.status = 500;
   }
