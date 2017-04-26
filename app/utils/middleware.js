3e43a1ae954ab31aaedf428b742239fe9733e1b7
Fanny <fannyvr18@gmail.com>
2017-04-25 21:31:54 -0700

    feat(login/out): created middleware function to check jwt expiration

--- app/utils/middleware.js
+++ app/utils/middleware.js
@@ -1,17 +1,27 @@
+import jwt from 'jwt-decode';
 import lock from './authInstance';
+
+const token = localStorage.getItem( 'id_token' );
 
 export const showLock = () => {
   lock.show();
 };
 
 export const noToken = singedOn => {
-  if( !localStorage.getItem( 'id_token' ) && !singedOn ){
+  if( !token && !singedOn ){
     return true;
   }
 };
 
 export const ifToken = singedOn => {
-  if( localStorage.getItem( 'id_token' ) && !singedOn ){
+  if( token && !singedOn ){
+    return true;
+  }
+};
+
+export const tokenExpired = () => {
+  const decoded = jwt(token);
+  if( decoded.exp < Date.now() / 1000 ){
     return true;
   }
 };
